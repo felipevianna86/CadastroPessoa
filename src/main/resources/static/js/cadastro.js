1bd4a65a2d653664d98fc962d72542cf38790684
Felipe <felipe.vianna86@gmail.com>
2020-03-04 20:52:40 -0300

    #12 - Adicionando máscara à data de nascimento.

--- src/main/resources/static/js/cadastro.js
+++ src/main/resources/static/js/cadastro.js
@@ -1,19 +1,28 @@
-$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event){
-	
-	var button = $(event.relatedTarget);
-	
-	var idPessoa = button.data('id');
-	var nome = button.data('nome');
-	
-	var modal = $(this);
-	var form = modal.find('form');
-	var action = form.data('url-base');
-	if(!action.endsWith('/')){
-		action += '/';
+function MascaraCPF(campo, e) {
+	if ((e.which >= 48) && (e.which <= 57)) {
+		if (campo.value.length == 3 || campo.value.length == 7) {
+			campo.value += '.';
+		} else if (campo.value.length == 11) {
+			campo.value += '-';
+		}
+		return true;
+	} else if (e.which == 0 || e.which == 8 || e.which == 127){
+		return true;
+	} else {
+		return false;
 	}
-	
-	form.attr('action', action + idPessoa);
-	
-	modal.find('.modal-body span').html('Tem certeza que deseja remover a pessoa <strong>'+nome+'</strong> ?');
-});
+}
+		
+function MascaraData(campo, e) {
+	if (e.which == 0 || e.which == 8 || e.which == 127){
+		return true;
+	} else if ((campo.value.length == 2 || campo.value.length == 5) && e.which != 47) {
+		campo.value += '/';
+	} else if (e.which == 47) {
+		return false;
+	} else if (campo.value.length == 10) {
+		return false;
+	}
+	return true;
+}
 
